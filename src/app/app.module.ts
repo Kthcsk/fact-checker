ae366bdec45d9f418393f2805e374bc1851910b5
Your Name <you@example.com>
2020-08-25 12:22:09 -0400

    Components, Carpeta Image y routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,73 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { Level1Component } from './components/level1/level1.component';
+import { Level2Component } from './components/level2/level2.component';
+import { Level3Component } from './components/level3/level3.component';
+import { Level4Component } from './components/level4/level4.component';
+import { WrongComponent } from './components/wrong/wrong.component';
+import { CorrectComponent } from './components/correct/correct.component';
+
+import { RouterModule, Routes } from '@angular/router';
+
+
+const route: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'menu',
+    component: MenuComponent
+  },
+  {
+    path: 'level1',
+    component: Level1Component
+  },
+  {
+    path: 'level2',
+    component: Level2Component
+  },
+  {
+    path: 'level3',
+    component: Level3Component
+  },
+  {
+    path: 'level4',
+    component: Level4Component
+  },
+  {
+    path: 'wrong',
+    component: WrongComponent
+  },
+  {
+    path: 'correct',
+    component: CorrectComponent
+  },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    Level1Component,
+    Level2Component,
+    Level3Component,
+    Level4Component,
+    WrongComponent,
+    CorrectComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(route)
   ],
   providers: [],
   bootstrap: [AppComponent]
