a46e5b55315aa0b4e4bf0af0c16aff1425bf00b1
AleKristen <correo@gmail.com>
2020-09-02 10:12:01 -0300

    level1

--- src/app/components/level1/level1.component.ts
+++ src/app/components/level1/level1.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-level1',
@@ -7,10 +8,85 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Level1Component implements OnInit {
 
-  constructor() { }
+  question: any;
+  questions: Array<{}> = [
+    {
+      id: 0,
+      title: "Las personas con asma tienen mayor riesgo ante el COVID-19",
+      options: [
+        {
+          a:"Verdadero",
+          b:"Falso"
+        }
+      ]
+    },
+      {
+        id: 1,
+        title: "Las infusiones con miel, limón o jengibre ¿curan el COVID-19?",
+        options: [
+          {
+            a:"Verdadero",
+            b:"Falso"
+          }
+      ]
+    },
+      {
+        id: 2,
+        title: "¿Las moscas domésticas pueden transmitir el COVID-19?",
+        options: [
+          {
+            a:"Verdadero",
+            b:"Falso"
+          }
+        ]
+      },
+      {
+        id: 3,
+        title: "¿El coronavirus puede afectar a personas jóvenes?",
+        options: [
+          {
+            a:"Verdadero",
+            b:"Falso"
+          }
+      ]
+    },
+    {
+      id: 4,
+      title: "Los termómetros sin contacto NO detectan el COVID-19",
+      options: [
+        {
+          a:"Verdadero",
+          b:"Falso"
+        }
+    ]
+    },
+    {
+      id: 5,
+      title: "¿Rociar el cuerpo con alcohol y cloro es efectivo para eliminar el Coronavirus? ",
+      options: [
+        {
+          a:"Verdadero",
+          b:"Falso"
+        }
+    ]
+    },
+      ];
+
+  index:any=0;
+
+  constructor(private router: Router) {
+  }
+
+  nextQuestion(){
+    if (this.index <= this.questions.length -1) {
+      return this.question = this.questions[(this.index++)];
+    } else {
+      this.router.navigateByUrl('/correct');
+    }
+  }
 
   ngOnInit(): void {
-    console.log('holi')
+    this.question = this.questions[(this.index)];
   }
 
 }
