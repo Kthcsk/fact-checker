0cebdfb81d76c911215fb86502a850ea87aa192a
Your Name <you@example.com>
2020-09-02 16:28:05 -0400

    true & false

--- src/app/components/level2/level2.component.ts
+++ src/app/components/level2/level2.component.ts
@@ -14,11 +14,9 @@ export class Level2Component implements OnInit {
       "number": 1,
       "question": "¿Qué famoso alimento ha sido falsamente señalado como una cura del virus?",
       "options": [
-        {
-          "a": "Choclo",
-          "b": "Ajo",
-          "c": "Lechuga"
-        }
+           "Choclo",
+           "Ajo",
+          "Lechuga"
       ],
       "answer": "Ajo",
       "explanation": "El ajo es un alimento saludable y posee propiedades antimicrobianas, pero no hay evidencia de que comer ajo proteja a nadie respecto a la infección por coronavirus."
@@ -27,11 +25,9 @@ export class Level2Component implements OnInit {
       "number": 2,
       "question": "Con el confinamiento se pretende…",
       "options": [
-        {
-          "a": "Aplanar la curva",
-          "b": "Descansar en casa",
-          "c": "Pasar más tiempo con la familia"
-        }
+          "Aplanar la curva",
+          "Descansar en casa",
+          "Pasar más tiempo con la familia"
       ],
       "answer": "Aplanar la curva",
       "explanation": "Falta esto"
@@ -40,24 +36,20 @@ export class Level2Component implements OnInit {
       "number": 3,
       "question": "¿Cuántas cepas de Coronavirus relacionados con enfermedades respiratorias en humanos existen?",
       "options": [
-        {
-          "a": 10,
-          "b": 5,
-          "c": 7
-        }
+          "10",
+          " 5",
+          "7"
       ],
-      "answer": 7,
+      "answer": "7",
       "explanation": "Los coronavirus son una extensa familia de virus que pueden causar enfermedades tanto en animales como en humanos. En los humanos, se sabe que varios coronavirus causan infecciones respiratorias que pueden ir desde el resfriado común hasta enfermedades más graves como el síndrome respiratorio de Oriente Medio (MERS) y el síndrome respiratorio agudo severo (SRAS). El coronavirus que se ha descubierto más recientemente causa la enfermedad por coronavirus COVID-19."
     },
     {
       "number": 4,
       "question": "¿Hasta cuántos días se estima que demora en incubar el virus en el cuerpo?",
       "options": [
-        {
-          "a": "10 días",
-          "b": "14 días",
-          "c": "7 días"
-        }
+           "10 días",
+          "14 días",
+           "7 días"
       ],
       "answer": "14 días",
       "explanation": "El “período de incubación” es el tiempo que transcurre entre la infección por el virus y la aparición de los síntomas de la enfermedad. La mayoría de las estimaciones respecto al periodo de incubación de COVID-19 oscilan entre 1 y 14 días, y en general se sitúan en torno a cinco días."
@@ -66,24 +58,21 @@ export class Level2Component implements OnInit {
       "number": 5,
       "question": "¿Cuántos días debe mantener una cuarentena alguien que estuvo en contacto con una persona con COVID-19?",
       "options": [
-        {
-          "a": "20 días",
-          "b": "7 días",
-          "c": "14 días"
-        }
+           "20 días",
+           "40 días",
+           "15 días"
       ],
-      "answer": "14 días",
+      "answer": "15 días",
       "explanation": "El aislamiento domiciliario (cuarentena) durará 14 días, desde que estuvo en contacto con una persona con COVID-19 en su periodo sintomático o desde la fecha de salida de un país que tiene un brote activo de COVID-19. El tiempo del aislamiento domiciliario (cuarentena) está determinado por el periodo de incubación del SARS-CoV-2."
     },
     {
       "number": 6,
       "question": "¿Cómo se llama al síntoma de dificultad para respirar?",
-      "options":
-      {
-        "a": "Disnea",
-        "b": "Cefalea",
-        "c": "Hiperventilación"
-      },
+      "options":[
+         "Disnea",
+        "Cefalea",
+        "Hiperventilación"
+    ],
       "answer": "Disnea",
       "explanation": "Pendiente"
     }
@@ -91,7 +80,11 @@ export class Level2Component implements OnInit {
 
   index: any = 0;
   answer: any;
+  options: any;
 
+
+  correctOption: Object;
+  wrongOption: Object;
 
   constructor(private router: Router) { }
 
@@ -100,6 +93,15 @@ export class Level2Component implements OnInit {
       return this.question = this.questions[(this.index++)];
     } else {
       this.router.navigateByUrl('/correct');
+    }
+  }
+  getAnswer(option:any) {
+    if(option === this.question.answer) {
+      this.correctOption = option;
+      this.wrongOption = {};
+    } else {
+      this.wrongOption = option;
+      this.correctOption = {};
     }
   }
 
